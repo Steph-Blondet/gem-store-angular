ae7e15c00976f3f521b401fd6921650dbb784724
stephanieblondethernandez <stephanie.blondet@gmail.com>
2017-03-07 10:03:40 -0500

    accepting submissions

--- app/app.js
+++ app/app.js
@@ -1,8 +1,27 @@
 (function(){
     var app = angular.module('gemStore', []);
 
-    app.controller('StoreController', function(){
+    app.controller("StoreController", function(){
         this.products = gems;
+    });
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller("ReviewController", function(){
+        this.review = {};
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        };
     });
 
     var gems = [
@@ -50,15 +69,6 @@
         }
     ];
 
-    app.controller("PanelController", function(){
-        this.tab = 1;
 
-        this.selectTab = function(setTab){
-            this.tab = setTab;
-        };
-        this.isSelected = function(checkTab){
-            return this.tab === checkTab;
-        };
-    });
 
 })();
