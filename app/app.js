bb23c09c31c973684e85dcb6f150cf589eb3e9f0
stephanieblondethernandez <stephanie.blondet@gmail.com>
2017-03-07 12:46:15 -0500

    custom directives

--- app/app.js
+++ app/app.js
@@ -1,73 +1,82 @@
 (function(){
-    var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', []);
 
-    app.controller("StoreController", function(){
-        this.products = gems;
-    });
+  app.directive('productDescription', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
 
-    app.controller("PanelController", function(){
-        this.tab = 1;
+  app.controller("StoreController", function(){
+    this.products = gems;
+  });
 
-        this.selectTab = function(setTab){
-            this.tab = setTab;
-        };
-        this.isSelected = function(checkTab){
-            return this.tab === checkTab;
-        };
-    });
+  app.controller("PanelController", function(){
+    this.tab = 1;
 
-    app.controller("ReviewController", function(){
-        this.review = {};
-        this.addReview = function(product){
-            product.reviews.push(this.review);
-            this.review = {};
-        };
-    });
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
 
-    var gems = [
-        {
-            name: 'Dodecahedron',
-            price: 2,
-            description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-            images: [
-                "img/gem-02.gif",
-                "img/gem-05.gif",
-                "img/gem-09.gif"
-            ],
-            reviews: [{
-                stars: 5,
-                body: "I love this gem!",
-                author: "joe@thomas.com",
-                createdOn: 1397490980837
-            }, {
-                stars: 1,
-                body: "This gem sucks.",
-                author: "tim@hater.com",
-                createdOn: 1397490980837
-            }]
-        },
-        {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-            images: [
-                "img/gem-01.gif",
-                "img/gem-03.gif",
-                "img/gem-04.gif",
-            ],
-            reviews: [{
-                stars: 3,
-                body: "I think this gem was just OK, could honestly use more shine, IMO.",
-                author: "JimmyDean@sausage.com",
-                createdOn: 1397490980837
-            }, {
-                stars: 4,
-                body: "Any gem with 12 faces is for me!",
-                author: "gemsRock@alyssaNicoll.com",
-                createdOn: 1397490980837
-            }]
-        }
-    ];
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller("ReviewController", function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      images: [
+        "img/gem-02.gif",
+        "img/gem-05.gif",
+        "img/gem-09.gif"
+      ],
+      reviews: [{
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@thomas.com",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "This gem sucks.",
+          author: "tim@hater.com",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+        images: [
+          "img/gem-01.gif",
+          "img/gem-03.gif",
+          "img/gem-04.gif",
+        ],
+        reviews: [{
+            stars: 3,
+            body: "I think this gem was just OK, could honestly use more shine, IMO.",
+            author: "JimmyDean@sausage.com",
+            createdOn: 1397490980837
+          }, {
+            stars: 4,
+            body: "Any gem with 12 faces is for me!",
+            author: "gemsRock@alyssaNicoll.com",
+            createdOn: 1397490980837
+        }]
+      }
+  ];
+
 
 
 
